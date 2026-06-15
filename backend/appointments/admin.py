02cba31b4304f338bbf605ffafcaffd5d59e55fe
LakshmiPriya-22 <lakshmipriya6177@gmail.com>
2026-06-15 11:44:29 +0530

    feat: register appointment model in Django admin

--- backend/appointments/admin.py
+++ backend/appointments/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Appointment
 
-# Register your models here.
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ['token_number', 'name', 'service_type', 'appointment_date', 'appointment_time', 'status']
+    list_filter = ['status', 'service_type', 'appointment_date']
+    search_fields = ['name', 'phone', 'token_number']
