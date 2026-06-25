5d0d8b47de645463c9195f22c4d6be09879d495b
LakshmiPriya-22 <lakshmipriya6177@gmail.com>
2026-06-25 15:12:21 +0530

    chore: add auth URLs to main URL config

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -20,5 +20,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/appointments/', include('appointments.urls')),
+    path('api/auth/', include('authentication.urls')),
+
 
 ]
